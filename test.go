f144e06179fd8d2e9b0d6f439a06e732847c9a56
ericsu <jd1006312@gmail.com>
2022-02-08 17:05:04 +0800

    tset

--- test.go
+++ test.go
@@ -12,16 +12,21 @@ import (
 	"bytes"
 	"os"
 	"io/ioutil"
+	"strings"
+	"path/filepath"
 )
+
+
 var (
 	/*addr     = flag.String("addr", ":8080", "TCP address to listen to")*/
 	/*host     = flag.String("host", "test", "TCP address to listen to")*/
 	compress = flag.Bool("compress", false, "Whether to enable transparent response compression")
 )
+
 func main() {
-	
+
+
 	h := requestHandler
-	
 	if *compress {
 		h = fasthttp.CompressHandler(h)
 	}
@@ -33,32 +38,57 @@ func main() {
 
 var addr string
 var host string
-var defaultImage image.Image
 var defaultConfig image.Config
 var defaultType string
+var defaultImageMap map[string]*defaultImg
+var imgSize int
+var defaultImage image.Image
+
+type defaultImg struct {
+    defaultImage image.Image
+    defaultConfig image.Config
+	defaultType string
+}
+
 func init(){
-//defaulte img
+    //defaulte img
     flag.Parse()
 	fmt.Printf("args=%s, num=%d\n", flag.Args(), flag.NArg())
 	addr = "localhost:" + flag.Args()[0]
 	host = flag.Args()[1]
-	defaultImage, defaultConfig, defaultType, _ = getImageFromFilePath("./default_img/test.jpg")
-	fmt.Printf(defaultType)
+
+	files, err := ioutil.ReadDir("./default_img/")
+    if err != nil {
+        log.Fatal(err)
+    }
+	
+	defaultImageMap = make(map[string]*defaultImg)
+    for _, file := range files {
+		defaultImage, defaultConfig, defaultType, imgSize, _ = getImageFromFilePath("./default_img/" + file.Name())
+		fileName := strings.TrimSuffix(file.Name(), filepath.Ext(file.Name()))
+		defaultImageMap[fileName] = &defaultImg{
+			defaultImage : defaultImage,
+			defaultConfig : defaultConfig,
+			defaultType : defaultType,
+		}
+		log.Printf("%d", imgSize)
+    }
 }
 
-func getImageFromFilePath(filePath string) (image.Image, image.Config, string, error) {
+func getImageFromFilePath(filePath string) ( image.Image, image.Config, string, int, error) {
     f, err := os.Open(filePath)
 	imgBytes, _ := ioutil.ReadAll(f);
     if err != nil {
         return nil,image.Config{},"", err
     }
-    defer f.Close()
     img, imageType, err := image.Decode(bytes.NewReader(imgBytes))
 	im, _, _ := image.DecodeConfig(bytes.NewReader(imgBytes))
-	log.Println(im.Width,im.Height)
-	log.Println(imageType)
-    return img, im, imageType, err
+	fmt.Printf("The file is %d bytes long\n", len(imgBytes))
+	defer f.Close()
+    return img, im, imageType , 123, err
 }
+
+
 
 
 func requestHandler(ctx *fasthttp.RequestCtx) {
@@ -69,25 +99,34 @@ func requestHandler(ctx *fasthttp.RequestCtx) {
 	var scale float64
 	query := string(ctx.QueryArgs().Peek("q"))
 	defaultQuery := string(ctx.QueryArgs().Peek("default"))
-	fmt.Printf( "defaultQuery is %q\n\n", defaultQuery)
+	method := string(ctx.QueryArgs().Peek("method"))
+	max := string(ctx.QueryArgs().Peek("max"))
+	maxNum, _:= strconv.Atoi(max)
+	maxNum = maxNum*1000
 
 	if query != ""{
 		var num string = query
-		scale, _ = strconv.ParseFloat(num, 64) 
+		scale, _ = strconv.ParseFloat(num, 64)
 	}else{
 		scale = 1
 	}
-	
-	fmt.Printf( "scale %f\n\n", scale)
-
 	resp := doRequest(host)
 	bodyBytes := resp.Body()
+
+	fmt.Printf("comapre")
+	fmt.Printf("%d %d\n",len(bodyBytes),maxNum)
+	if method == "UB" && len(bodyBytes) > maxNum{
+		divNum := len(bodyBytes) * maxNum
+		scale = float64(divNum)
+		fmt.Printf("UB")
+		fmt.Printf("%f",scale)
+	}
 
 	var respHeader fasthttp.ResponseHeader
 	var contentType string
 	var img image.Image
 	var im image.Config
-
+	
 	if resp.StatusCode() == 200{
 		respHeader = resp.Header
 		contentType = string(respHeader.ContentType())
@@ -97,16 +136,18 @@ func requestHandler(ctx *fasthttp.RequestCtx) {
 		send_s3 := compressImg(img, im, scale, contentType)
 		response(ctx, send_s3, contentType)
 	}else if resp.StatusCode() != 200 && defaultQuery != ""{
-		contentType = "image/" + defaultType
-		log.Println("resp.StatusCode = ",resp.StatusCode())
-		log.Println("defaultQuery = ",defaultQuery)
-		img = defaultImage
-		im = defaultConfig
+		contentType = "image/" + defaultImageMap[defaultQuery].defaultType
+		//log.Println("resp.StatusCode = ",resp.StatusCode())
+		//log.Println("defaultQuery = ",defaultQuery)
+		img = defaultImageMap[defaultQuery].defaultImage
+		im = defaultImageMap[defaultQuery].defaultConfig
 		send_s3 := compressImg(img, im, scale, contentType)
 		response(ctx ,send_s3, contentType)
 	}else{
 		log.Println("resp.StatusCode = ",resp.StatusCode())
 		log.Println("err")
+		log.Println(fasthttp.StatusUnsupportedMediaType )
+		ctx.Error("Unsupported path", fasthttp.StatusUnsupportedMediaType )
 	}
 	fmt.Printf("size %d %d\n\n", im.Width, im.Height)
 }
@@ -114,7 +155,7 @@ func requestHandler(ctx *fasthttp.RequestCtx) {
 func response(ctx *fasthttp.RequestCtx, send_s3 []byte, contentType string){
 
 	ctx.SetContentType(contentType)
-	
+
 	ctx.SetBody([]byte(send_s3))
 
 	ctx.Response.Header.Set("X-My-Header", "my-header-value")
@@ -126,7 +167,7 @@ func response(ctx *fasthttp.RequestCtx, send_s3 []byte, contentType string){
 }
 
 func compressImg(img image.Image, im image.Config, scale float64, contentType string)([]byte){
-		
+	fmt.Printf( "origin size %d %d\n\n",im.Height, im.Width)
 	h :=  float64(im.Height)*scale
 	w :=  float64(im.Width)*scale
 	fmt.Printf( "reize %f %f\n\n",h, w)
